0cc8c11138d6dbf16cc584659984c1ef97a248af
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-03 11:08:34 +0530

    Complete 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function(str, num) {
+    let myString = str;
+    for(i = 1; i < num; i++){
+    myString += str;
+    }
+    if (num === 0){
+    return "";
+    } else if (num < 0){
+    return 'ERROR';
+    }
+    else{
+    return myString;
+    
+    }
+    };
+    
+    // Do not edit below this line
+    module.exports = repeatString;
+    
+    
