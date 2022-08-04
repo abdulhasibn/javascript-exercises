1724969f92ba36e111c57bca034a2bc0bf4c1af0
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-04 12:13:26 +0530

    Complete 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let i = num1;
+    if ((num1 < 0 || num2 < 0) || !(Number.isInteger(num1) &&  Number.isInteger(num2))){
+        return "ERROR";
+    } 
+    else{
+        if(num1 > num2){
+            i = num2;
+            do{
+                sum += i;
+                i++;
+        } while(i < num1);
+        }
+        do{
+            sum += i;
+            i++;
+        } while(i <= num2);
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
