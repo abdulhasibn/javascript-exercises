060a6b05e252daf992cc0b211946b3de88a300fb
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-04 10:47:14 +0530

    Complete 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius;
+  celsius = (fahrenheit - 32) * (5/9);
+  return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit;
+  fahrenheit = (celsius * (9/5) + 32);
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
