092ee6528e62bfb657f155fd959d4a4f4fd869a3
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-04 06:58:04 +0530

    Complete 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+    let myArray = arr;
+    let mySecondArray = num;
+    for(i = 0; i <mySecondArray.length ; i++){
+        if (myArray.indexOf(mySecondArray[i]) >= 0){
+            myArray.splice(myArray.indexOf(mySecondArray[i]), 1);
+        } else{
+            continue;
+        }
+    }
+    return myArray;
 
+    
 };
 
 // Do not edit below this line
