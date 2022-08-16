1463dde00277687099ac07c9e1dc36e5f66c9694
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-16 19:32:13 +0530

    Complete 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
 const fibonacci = function(num) {
-    
+    if(num < 0){
+        return "OOPS";
+    }
+    const a = 1;
+    const b = 1;
+    let myArray = [];
+    myArray.push(a);
+    myArray.push(b);
+    for (i = 2; i < Number(num); i++){
+        let c = myArray[i-1] + myArray[i-2];
+        myArray.push(c);
+    }
+    return myArray[myArray.length-1];
+
 };
 
 // Do not edit below this line
