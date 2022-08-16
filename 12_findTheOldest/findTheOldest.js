e92094b144ac6f9debe1a25463d3bd38e0e60a46
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-16 11:22:38 +0530

    Complete 12 of the javascript exercises repo

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const ages = arr.map(item => {
+        if(item.yearOfDeath === undefined){
+            const currentYear = (new Date()).getFullYear();
+            return currentYear - item.yearOfBirth;
+        }
+        return item.yearOfDeath - item.yearOfBirth;
+    });
+    let sortedAges = ages.sort((a, b) => a - b);
+    const oldestAge = sortedAges[sortedAges.length - 1];
+    const oldestPerson = arr.map(item => {
+        const currentYear = (new Date()).getFullYear();
+        if(oldestAge === (item.yearOfDeath - item.yearOfBirth)){
+            return item;
+        } else if(oldestAge === ( currentYear- item.yearOfBirth)){
+            return item;
+        }
+    })
+    for (i = 0; i < oldestPerson.length; i++){
+        if (oldestPerson[i] !== undefined){
+            return oldestPerson[i];
+        } else{
+            continue;
+        }
+    }
 };
 
 // Do not edit below this line
