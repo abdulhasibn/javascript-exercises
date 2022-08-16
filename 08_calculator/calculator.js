8e46642ae57c3e27d6d187aee9153abccde041ab
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-16 08:01:37 +0530

    Complete 08 exercise of javascript exercises repo

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b, ...args) {
+	return parseInt(a + b + Number(args));
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b, ...args) {
+	return parseInt(a - b - Number(args));
 };
 
-const sum = function() {
-	
+const sum = function(myArr) {
+  let sum = 0;
+  myArr.map(myInt => sum += myInt);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(myArr) {
+  let sum = 1;
+  myArr.map(myInt => sum *= myInt);
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0){
+    return 1;
+  }
+	return num *= factorial(num -1);
 };
 
 // Do not edit below this line
