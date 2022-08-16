08ace29a20766c87146ec1835c1e30fe8f01508d
Abdul Hasib <abdulhasibn@gmail.com>
2022-08-16 08:57:24 +0530

    Complete 09 of javascript exercises repo

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const oldStr = str.toLowerCase().replace(/[^a-z]/g, '');
+    const newStr = oldStr.split('').reverse().join('');
+    return oldStr === newStr;
 };
 
 // Do not edit below this line
